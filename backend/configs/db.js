663558d39ae52d56498bd9f723954cf364d1790e
2shining <fetrafaneva@gmail.com>
2026-02-07 18:23:04 +0300

    feat: configuration of the backend express

---
+++ backend/configs/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connectecd", () => {
+      console.log("Database connected successfully");
+
+      let mongodbURI = process.env.MONGODB_URI;
+      const projectName = "content-api-backend";
+    });
+
+    if (!mongodbURI) {
+      throw new Error("MONGODB_URI environment variable not set");
+    }
+
+    if (mongodbURI.endsWith("/")) {
+      mongodbURI = mongodbURI.slice(0, -1);
+    }
+
+    await mongoose.connect(`${mongodbURI}/${projectName}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+  }
+};
+
+export default connectDB;
